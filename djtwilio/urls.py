cb57c1170def4ff2a0ad6722aab0695afd9a3d46
Paul Hallett <hello@phalt.co>
2014-06-27 15:45:43 +0100

    Adding respond url handler

--- djtwilio/urls.py
+++ djtwilio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #Here we add our Twilio URLs
     url(r'^gather/$', 'djtwilio.views.gather_digits'),
+    url(r'^respond/$', 'djtwilio.views.handle_response'),
 )
