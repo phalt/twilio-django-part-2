e41986a852b3a03adc67cd25aa8cadf1e380cde7
Paul Hallett <hello@phalt.co>
2014-06-27 13:50:55 +0100

    Updating settings properly

--- djtwilio/settings.py
+++ djtwilio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TWILIO_FORGERY_PROTECTION=False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_twilio',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
