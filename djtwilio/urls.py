5d9f838edc228a39c4d0bf02fc87b5f9f6e7038f
Paul Hallett <hello@phalt.co>
2014-06-27 13:34:15 +0100

    Added URL for gather_digits to urlconf

--- djtwilio/urls.py
+++ djtwilio/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #Here we add our Twilio URLs
+    url(r'^gather/$', 'djtwilio.views.gather_digits'),
 )
