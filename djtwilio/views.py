bd5a080eaa871aef76343fc973a1ff94d05a4bd5
Paul Hallett <hello@phalt.co>
2014-07-07 14:43:31 +0100

    Add handle_response view

--- djtwilio/views.py
+++ djtwilio/views.py
@@ -13,3 +13,20 @@ def gather_digits(request):
         g.say('Press one to hear a song, two to receive an SMS')
 
     return r
+
+@twilio_view
+def handle_response(request):
+
+    digits = request.POST.get('Digits', '')
+
+    twilio_response = Response()
+
+    if digits == '1':
+        twilio_response.play('http://bit.ly/phaltsw')
+
+    if digits == '2':
+        number = request.POST.get('From', '')
+        twilio_response.say('A text message is on its way')
+        twilio_response.sms('You looking lovely today!', to=number)
+
+    return twilio_response
