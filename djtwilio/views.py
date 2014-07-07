f55b9a3c72b0c4423630adabf005507a17f43f6e
Paul Hallett <hello@phalt.co>
2014-07-07 14:43:12 +0100

    bump

--- djtwilio/views.py
+++ djtwilio/views.py
@@ -13,21 +13,3 @@ def gather_digits(request):
         g.say('Press one to hear a song, two to receive an SMS')
 
     return r
-
-@twilio_view
-def handle_response(request):
-
-    digits = request.POST.get('Digits', '')
-
-    r = Response()
-
-    if digits == '1':
-        r.play('http://bit.ly/phaltsw')
-
-    if digits == '2':
-        number = request.POST.get('From', '')
-        r.say('A text message is on its way')
-        r.sms('You looking lovely today!', to=number)
-
-    return r
-
