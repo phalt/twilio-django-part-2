54e38fa964ed8418f29fff2bce0682f798812632
Paul Hallett <hello@phalt.co>
2014-07-16 10:49:51 +0100

    Fix admin urls

--- djtwilio/urls.py
+++ djtwilio/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #  Here we add our Twilio URLs
