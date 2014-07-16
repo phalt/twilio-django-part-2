6986603fe9664b682c9209635ee9c4650123297d
Paul Hallett <hello@phalt.co>
2014-07-16 10:47:24 +0100

    Replaced Admin URLs

--- djtwilio/urls.py
+++ djtwilio/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    #  Here we add our Twilio URLs
     url(r'^gather/$', 'djtwilio.views.gather_digits'),
     url(r'^respond/$', 'djtwilio.views.handle_response'),
 )
